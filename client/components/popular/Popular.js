c7f71ebce202ad0777ac294d6657ed4d9463b879
Michael Cockcroft <info@likemike.me>
2016-11-01 23:14:50 +0000

    Adding intro and and small ui amends
    - Added intro
    - Amended data labels on popular data
    - Updated tests
    - Added theme file for consistent styling

--- client/components/popular/Popular.js
+++ client/components/popular/Popular.js
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
+import Loader from '../Loader';
 
 class Popular extends Component {
 
   renderKey(key) {
     const { mostPopular } = this.props;
-    return (
-      mostPopular[key] &&
+    if (mostPopular[key] && (key !== 'ict_index')) {
+      return (
         <div key={key}>
-          <h2>{mostPopular[key].web_name || ''}</h2>
-          <p>{key} {mostPopular[key][key] || ''}</p>
+          <p>{this.getTitle(key)}</p>
+          <h2>{mostPopular[key].web_name || ''} {mostPopular[key][key] || ''}</h2>
         </div>
-    );
+      );
+    }
+  }
+
+  getTitle(key) {
+    switch(key) {
+      case 'selected_by_percent':
+        return 'Most Selected';
+      case 'total_points':
+        return 'Highest Points';
+      case 'transfers_in_event':
+        return 'Most Transferred In';
+      case 'transfers_out_event':
+        return 'Most Transferred Out';
+      case 'form':
+        return 'Form';
+      case 'value_form':
+        return 'Best Value/Form Balance';
+    }
   }
 
   render() {
@@ -20,7 +39,7 @@ class Popular extends Component {
       <div>
         <ul>
           {!mostPopular &&
-            <p className="loader">...loading</p>
+            <Loader />
           }
           {mostPopular &&
             Object.keys(mostPopular || {}).map((key) => {
--- test/App.test.js
+++ test/App.test.js
@@ -5,6 +5,7 @@ import { mount, shallow } from 'enzyme';
 
 import App from '../client/App';
 import Header from '../client/components/Header';
+import Intro from '../client/components/Intro';
 import PopularContainer from '../client/components/popular/PopularContainer';
 
 describe('App component', function() {
@@ -12,6 +13,11 @@ describe('App component', function() {
   it("contains <Header /> component", function() {
     const app = shallow(<App />)
     expect(app.find(Header)).to.have.length(1);
+  });
+
+  it("contains <Intro /> component", function() {
+    const app = shallow(<App />)
+    expect(app.find(Intro)).to.have.length(1);
   });
 
   it("contains <PopularContainer /> component", function() {
