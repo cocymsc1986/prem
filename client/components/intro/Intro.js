0f4aa86791a98a8b2ca73fc509edc97c039dc989
Michael Cockcroft <info@likemike.me>
2018-01-08 22:44:29 +0000

    intro-data
    - Added real data to intro component

---
+++ client/components/intro/Intro.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {StyleSheet, css} from 'aphrodite';
+import {font, maxWidth} from '../../ui/theme';
+import Loader from '../Loader';
+
+const styles = StyleSheet.create({
+  intro: {
+    fontSize: font.size.lead,
+    lineHeight: font.size.leadValue * 1.5 + 'px',
+    maxWidth: maxWidth,
+    margin: '0 auto'
+  }
+});
+
+class Intro extends Component {
+  render() {
+    const {error, data} = this.props;
+    const gameweek = data && data['current-event'];
+    const deadline = data && data.events[gameweek].deadline_time_formatted;
+
+    if (error) {
+      return (
+        <div className={css(styles.intro)}>
+          <p data-test='dataless'>
+            Welcome to Fantasy Prem stats site. This site surfaces current stats for
+            Premier League Fantasy Football in an easy to digest format. All points are
+            up to date as of this gameweek.
+          </p>
+        </div>
+      );
+    }
+
+    if (!data) {
+      return (
+        <Loader />
+      );
+    }
+
+    return (
+      <div className={css(styles.intro)}>
+        <p>
+          Welcome to Fantasy Prem stats site. This site surfaces current stats for
+          Premier League Fantasy Football in an easy to digest format. All points are
+          up to date as of gameweek {gameweek} for deadline {deadline}. Points will
+          next be updated at the completion of gameweek {gameweek + 1}.
+        </p>
+      </div>
+    );
+  }
+}
+
+Intro.propTypes = {
+  data: PropTypes.object,
+  error: PropTypes.object
+};
+
+export default Intro;
--- test/components/Intro.test.js
+++ test/components/Intro.test.js
@@ -4,11 +4,41 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import Intro from '../../client/components/Intro';
+import Intro from '../../client/components/intro/Intro';
+import Loader from '../../client/components/Loader';
 
-describe('Header component', function () {
-  it('contains <h1 /> element', function () {
-    const app = shallow(<Intro />);
-    expect(app.find('p')).to.have.length(1);
+describe('Intro component', function () {
+  const data = {
+    'current-event': 0,
+    events: [
+      {
+        deadline_time_formatted: '11 Aug 18:45'
+      }
+    ]
+  };
+
+  const error = {
+    error: 'test'
+  };
+
+  context('With error', () => {
+    it('should render dataless p', function () {
+      const app = shallow(<Intro error={error} data={data} />);
+      expect(app.find('[data-test="dataless"]')).to.have.length(1);
+    });
+  });
+
+  context('Without data', () => {
+    it('should render <Loader> component', function () {
+      const app = shallow(<Intro />);
+      expect(app.find(Loader)).to.have.length(1);
+    });
+  });
+
+  context('With data', () => {
+    it('should render <p> element', function () {
+      const app = shallow(<Intro data={data} />);
+      expect(app.find('p')).to.have.length(1);
+    });
   });
 });
